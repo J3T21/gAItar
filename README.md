f33e0e51e10452063538eab7ff211645a41481b7
james taylor <jameset701@gmail.com>
2024-12-15 18:54:41 +0000

    mini solenoid tested

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gAItar
 Final Year Masters Electronic and Information Engineering project: A mechatronic interface to play an acoustic guitar with some AI elements
+https://www.classicalguitarmidi.com/tarrega.html
---
+++ midi_map.py
@@ -0,0 +1,38 @@
+import mido
+
+
+
+def print_midi_file(file_path):
+    '''few notes about midi:
+    - midi files are composed of tracks
+    - each track is composed of messages
+    - each message is composed of a type and a value
+    - the type can be note_on, note_off, control_change, etc
+    - time is mesured in pulses per quarter note (ppqn)'''
+    mid = mido.MidiFile(file_path)
+    with open("midi_messages_nor.txt", "w") as f:
+        for i, track in enumerate(mid.tracks):
+            f.write(f"Track {i}: {track.name}\n")
+            for msg in track:
+                f.write(f"{msg}\n")
+    # for i, track in enumerate(mid.tracks):
+    #     print(f"Track {i}: {track.name}")
+    #     for msg in track:
+    #         if msg.is_meta:
+    #             print(msg)
+    # #mid.print_tracks()
+    # for i, track in enumerate(mid.tracks):
+    #     for msg in track:
+    #         if msg.type == 'program_change':
+    #             print(f"Track {i} Channel {msg.channel} Instrument {msg.program}")
+    for i, track in enumerate(mid.tracks):
+        new_file_path = f"track_{i}.mid"
+        new_mid = mido.MidiFile()
+        new_mid.tracks.append(track)
+        new_mid.save(new_file_path)
+        print(f"Saved track {i} as {new_file_path}")
+
+    #merged = mido.merge_tracks(mid.tracks)
+
+if __name__ == '__main__':
+    print_midi_file('midi_tracks/Norwegian-Wood.mid')
